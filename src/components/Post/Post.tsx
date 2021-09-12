de86fdef098cf2336bc28a922722cdb802bea1e6
snowfelix426 <felixleo426@gmail.com>
2021-09-12 10:24:18 -0400

    Update post component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -15,28 +15,27 @@ const Post = ({
   date,
   description,
   isImageLeft = true,
-  ...props
 }: IPostProps) => {
   return (
-    <div className="">
-      <div className="grid gap-14 lg:grid-cols-2">
-        <div className={`inline-flex ${isImageLeft ? '' : 'lg:order-last'}`}>
-          <Image
-            src={imageUrl}
-            alt="post-img"
-            width={480}
-            height={300}
-            objectFit="cover"
-          />
-        </div>
+    <div className="grid gap-6 lg:gap-14 lg:grid-cols-2">
+      <div className={`block ${isImageLeft ? '' : 'lg:order-last'}`}>
+        <Image
+          src={imageUrl}
+          alt="post-img"
+          width={480}
+          height={300}
+          objectFit="cover"
+          layout="responsive"
+          className="w-full"
+        />
+      </div>
 
-        <div className={`${isImageLeft ? 'text-left' : 'text-right'}`}>
-          <span className="text-sm lg:text-base text-dark-golden">{date}</span>
-          <h3 className="pt-1 mb-2 text-xl font-medium leading-none text-black lg:text-2xl lg:mb-4">
-            {name}
-          </h3>
-          <p className="text-sm text-light-grey">{description}</p>
-        </div>
+      <div className={`${isImageLeft ? '' : 'lg:text-right'}`}>
+        <span className="text-sm lg:text-base text-dark-golden">{date}</span>
+        <h3 className="pt-1 mb-2 text-xl font-medium leading-none text-black lg:text-2xl lg:mb-4">
+          {name}
+        </h3>
+        <p className="text-sm text-light-grey">{description}</p>
       </div>
     </div>
   );
