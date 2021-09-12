3971f0505a71b108efe7f7051741964d5ce21d30
snowfelix426 <felixleo426@gmail.com>
2021-09-12 09:14:40 -0400

    Add static content for hero section

---
+++ src/utils/textContent.ts
@@ -0,0 +1,11 @@
+const content = {
+  hero: {
+    backgroundImage: '/assets/images/img-hero-bg.png',
+    subtitle: 'Interview',
+    title: 'Catching up with Gold-Diggers',
+    ctaLabel: 'Get Started',
+    ctaLink: '/',
+  },
+};
+
+export default content;
